ed56c01dc157889c8a23924d0f240e0fbbf24ffb
gsp <gsaipavan006@gmail.com>
2023-12-26 11:21:18 +0530

    modified unit tests

--- src/test/java/com/anju/demo1/controller/CalculatorControllerTest.java
+++ src/test/java/com/anju/demo1/controller/CalculatorControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.mockito.Mockito.when;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -25,60 +26,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @ExtendWith(MockitoExtension.class)
-@WebMvcTest(CalculatorController.class)
 public class CalculatorControllerTest {
 
-   private MockMvc mockMvc;
+
 
     @Mock
     private CalculatorService calculatorService;
 
-    @InjectMocks
     private CalculatorController calculatorController;
 
     @BeforeEach
     public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(calculatorController).build();
+        calculatorController = new CalculatorController(calculatorService);
     }
 
     @Test
     public void testAddEndpoint() throws Exception {
         when(calculatorService.add(3, 5)).thenReturn(8);
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculator/add?a=3&b=5"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("8"));
+        int a = calculatorController.add(3,5);
+        assert(a==8);
     }
 
     @Test
     public void testSubtractEndpoint() throws Exception {
         when(calculatorService.subtract(10, 4)).thenReturn(6);
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculator/subtract?a=10&b=4"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("6"));
+        int a = calculatorController.subtract(10,4);
+        assert(a==6);
     }
 
     @Test
 
     public void testMultiplyEndpoint() throws Exception {
         when(calculatorService.multiply(3, 5)).thenReturn(15);
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculator/multiply?a=3&b=5"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("15"));
+        int a = calculatorController.multiply(3,5);
+        assert(a==15);
     }
 
     @Test
 
     public void testDivideEndpoint() throws Exception {
         when(calculatorService.divide(10, 2)).thenReturn(5.0);
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculator/divide?a=10&b=2"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("5.0"));
+        double a = calculatorController.divide(10,2);
+        assert(a==5.0);
     }
 
-    @Test
-    public void testDivideByZero() throws Exception {
-        when(calculatorService.divide(10, 0)).thenThrow(new IllegalArgumentException("cannot divide by zero"));
-        mockMvc.perform(MockMvcRequestBuilders.get("/calculator/divide?a=10&b=0"))
-                .andExpect(status().isBadRequest());
-    }
 }
